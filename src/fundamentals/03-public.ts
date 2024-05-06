e725a8baeae4c3003d767e9e3b65525be7aa7194
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-05-06 12:32:09 -0600

    public access

---
+++ src/fundamentals/03-public.ts
@@ -0,0 +1,31 @@
+export class MyDate {
+    year: number;
+    month: number;
+    day: number;
+  
+    constructor(year: number, month: number, day: number) {
+      this.year = year;
+      this.month = month;
+      this.day = day;
+    }
+  
+    printDate() {
+      return `${this.day}/${this.month}/${this.year}`;
+    }
+  
+    add(amount: number, type: "day" | "month" | "year") {
+      if (type === "day") {
+        this.day += amount;
+      } else if (type === "month") {
+        this.month += amount;
+      } else if (type === "year") {
+        this.year += amount;
+      }
+    }
+  }
+  
+  const myDate = new MyDate(1995, 7, 16);
+  console.log(myDate.day) // 16
+  myDate.day = 24
+  console.log(myDate.day) // 24
+  
