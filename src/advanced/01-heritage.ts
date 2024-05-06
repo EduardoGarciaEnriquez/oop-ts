8b4bf1bde74b54855b4de9421272e046c40f8fbe
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-05-06 16:27:45 -0600

    heritage

---
+++ src/advanced/01-heritage.ts
@@ -0,0 +1,25 @@
+export class Animal {
+  constructor(public name: string) {}
+
+  greeting(): string {
+    return `Hello my name is ${this.name}`;
+  }
+}
+
+export class Dog extends Animal {
+  //name no tiene acceso y lo encapsulamos en super porque pertenece a la clase de la que hereda
+  //no queremos definir una propiedad interna, solamente pasar el valor la constructor de la herencia
+  constructor(name: string, public owner: string) {
+    super(name);
+  }
+
+  bark(times: number):void {
+    for (let i = 0; i < times; i++) {
+      console.log("barf");
+    }
+  }
+}
+
+const chester = new Dog('chester', 'Edgar')
+chester.greeting()
+console.log(chester.owner)
