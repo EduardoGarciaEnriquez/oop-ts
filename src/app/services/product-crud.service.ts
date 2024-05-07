fd052a44aabe76a82fe6ac7056e409e55df65d87
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-05-07 16:43:08 -0600

    doc 🚧

--- src/app/services/product-crud.service.ts
+++ src/app/services/product-crud.service.ts
@@ -7,6 +7,7 @@ export class ProductCRUDService {
   private url = "https://api.escuelajs.co/api/v1/products/";
   private http = new BaseHttpService<Product>(this.url);
 
+  //async method that returns the method update from the http instance from baseHttpService.
   async update(id: Product["id"], changes: UpdateProductDto) {
     return this.http.update(id, changes);
   }
