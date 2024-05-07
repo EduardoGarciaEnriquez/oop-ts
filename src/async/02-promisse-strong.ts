93aafa9d37c811a73bdcc73f572d71d9fddc29fc
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-05-07 10:13:26 -0600

    fix: promisses-strong types

---
+++ src/async/02-promisse-strong.ts
@@ -0,0 +1,16 @@
+/*tipado fuerte en promesas*/
+import axios from "axios";
+import { Product } from "./models/product.model";
+
+(async () => {
+  // tipado hacia afuera, es decir tipar que es lo que va a retornar
+  async function getProducts(): Promise<Product[]> {
+    //tipado interno, es decir que espera recibir en el response del axios.get
+    const { data } = await axios.get<Product[]>(
+      "https://api.escuelajs.co/api/v1/products?limit=1&offset=0"
+    );
+    return data;
+  }
+
+  const products = await getProducts();
+})();
